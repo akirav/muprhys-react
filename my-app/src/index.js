6755fdc804a347fa8932378c939d1e1283977361
Akira Vernon <akirav@hawaii.edu>
2018-02-28 15:35:32 -1000

    Test

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Menu, Dropdown, Icon, Image} from 'semantic-ui-react';
 
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless style={{
+     border: 'none', boxShadow:'none',
 
+    }} className="topMenu">
+        <Container >
+        <Image src="http://murphyshawaii.com/media/2014/04/murphyshawaii.png"/>
+
+        <Menu.Item position="right" fitted>Home</Menu.Item>
+          <Dropdown item text="About Us" icon ="dropdown">
+            <Dropdown.Menu>
+              <Dropdown.Item>My Cart is currently empty.</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        <Menu.Item fitted>St. Patrick's Day</Menu.Item>
+        <Dropdown item text="Menus" icon ="dropdown">
+          <Dropdown.Menu>
+            <Dropdown.Item>My Cart is currently empty.</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Menu.Item >Bar</Menu.Item>
+          <Menu.Item fitted><Icon name = "search" /></Menu.Item>
+    </Container>
+    </Menu>
+  )
+  }
+}
+
+class Middle extends React.Component{
+  render(){
+    return(
+        <div className='murphys-background'>
+
+        </div>
+    )
+  }
+}
 
 
 class Murphys extends React.Component {
   render() {
     return (
-        <Container textAlign="center">
-    <Header as='h1'>Murphys</Header>
-        </Container>
+      <div>
+        <TopMenu/>
+        <Middle/>
+      </div>
   );
   }
 }
